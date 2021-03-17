35144bd6588c65312a8ca702169712ac8833897a
joy,zhou <joy.zhou@ucloud.cn>
2021-03-17 13:46:38 +0800

    add

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 python3 setup.py check
 python3 setup.py build
-sudo python3 setup.py install --force
+# sudo python3 setup.py install --force --install-dir /Users/yu/Library/Python/3.8/lib/python/site-packages
 sudo python3 setup.py sdist bdist_wheel || true
 sudo python3 -m twine upload dist/*.whl
 
 sudo rm -rf build
 sudo rm -rf dist
-sudo rm -rf iotedge_driver_link_sdk.egg-info
+sudo rm -rf iotedge_application_link_sdk.egg-info
 
